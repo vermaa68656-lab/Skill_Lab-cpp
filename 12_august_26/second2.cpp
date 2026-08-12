2b8323f98616eb032c025817aeca2c45504cd9f4
Ankit Verma <vermaa68656@gmail.com>
2026-08-11 22:04:36 -0700

    added 3 files

---
+++ 12_august_26/second2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    float a;
+    cout << "enter the value of a";
+    cin >> a;
+    cout << "value of a is : "<<a;
+    float b;
+    cout << "enter the value of b";
+    cin >>b,
+    cout <<"value of b is:"<<b ;
+    cout << " sum of a and b is " <<a + b;
+    return 0;
+}
---
+++ 12_august_26/second3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    double a;
+    cout << "enter the value of a";
+    cin >> a;
+    cout << "value of a is : "<<a;
+    double b;
+    cout << "enter the value of b";
+    cin >>b,
+    cout <<"value of b is:"<<b ;
+    cout << " sum of a and b is " <<a + b;
+    return 0;
+}
