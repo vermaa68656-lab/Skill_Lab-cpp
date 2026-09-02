d4af0f3a75cae6efc2963068c02881e64ffdd0a0
Ankit Verma <vermaa68656@gmail.com>
2026-09-01 21:51:07 -0700

    star and one dimension

---
+++ 2nd_sept/array1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s[5];
+    s[0]=90;
+    s[1]=80;
+    s[2]=90;
+    s[3]=70;
+    s[4]=60;
+    for(int i=0;i<5;i++)
+    {
+        cout<<s[i];
+        cout<<endl;
+    }
+    
+    return 0;
+}
